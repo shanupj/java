674802e8c022963cc6aa888c3e7bd9bed74145ea
shanupj <125428013+shanupj@users.noreply.github.com>
2023-06-07 15:12:55 +0530

    Update shapes.java

--- CO3/shapes.java
+++ CO3/shapes.java
@@ -13,7 +13,7 @@ public class shapes {
         	System.out.println("\nArea of Cuboid is with dimensions "+a1+" X "+b1+" X "+c1+" = "+Area_val);
 	}
 	public static void main(String[] args) {
-		System.out.println("Name: SHANU P J\nRegisterNo: SJC22MCA-2050\nCourse Code: 20MCA132\n Date: 24/03/2023");
+		System.out.println("Name: SHANU P J\nRegisterNo: SJC22MCA-2050\nCourse Code: 20MCA132\n Date: 02/06/2023");
         	Scanner sc = new Scanner(System.in);
         	System.out.println("\nEnter the Length");
         	int l = sc.nextInt();
